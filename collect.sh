f3dd1cd4c557452dd0e4ee332742cdffa92efb3c
Qingchuan Hao <qinhao@microsoft.com>
2020-12-29 11:15:31 +0000

    compress one level dir

--- collect.sh
+++ collect.sh
@@ -1,42 +1,45 @@
-#!/bin/bash
+#!/bin/sh
+BASEDIR=/mnt/npmlogs
+LOGDIRNAME=$HOSTNAME
+LOGDIR=$BASEDIR/$LOGDIRNAME
+COMPRESSEDLOGFILE=${LOGDIR}.tar.gz
+mkdir -p $LOGDIR
 
-HOSTDIR=/mnt/npmlogs/$HOSTNAME
-COMPRESSEDFILE=${HOSTDIR}.tar.gz
-mkdir -p $HOSTDIR
-
-echo "logs will be stored in $HOSTDIR"
-echo "start to collect log: $(date)" >> $HOSTDIR/date.txt
+echo "logs will be stored in $LOGDIR"
+echo "start to collect log: $(date)" >> $LOGDIR/date.txt
 
 echo "Dumping Azure Npm Logs begin"
-cat /var/log/azure-npm.log >> $HOSTDIR/azure-npm.log
+cat /var/log/azure-npm.log >> $LOGDIR/azure-npm.log
 
 echo "Dumping IPTABLES-RULES"
-iptables -vnL >> $HOSTDIR/iptables.txt
+iptables -vnL >> $LOGDIR/iptables.txt
 echo "Dumping IPTABLES-SAVE"
-iptables-save >> $HOSTDIR/iptables.txt
+iptables-save >> $LOGDIR/iptables.txt
 echo "Dumping IPSET LISTS"
-ipset list > $HOSTDIR/ipsetlist.txt
+ipset list > $LOGDIR/ipsetlist.txt
 echo "Dumping ROUTE LISTS"
-ip route list >> $HOSTDIR/iproute.txt
-ip rule >> $HOSTDIR/ipsetlist.txt
+ip route list >> $LOGDIR/iproute.txt
+ip rule >> $LOGDIR/ipsetlist.txt
 
 echo "Dumping kubectl network policy all name-spaces"
-kubectl get networkpolicy --all-namespaces >> $HOSTDIR/all-networkpolicies.txt
-kubectl get networkpolicy --all-namespaces -o yaml >> $HOSTDIR/all-networkpolicies.txt
+kubectl get networkpolicy --all-namespaces >> $LOGDIR/all-networkpolicies.txt
+echo "" >> $LOGDIR/all-networkpolicies.txt
+kubectl get networkpolicy --all-namespaces -o yaml >> $LOGDIR/all-networkpolicies.txt
 
 echo "Dumping kubectl get pods -o wide"
-kubectl get pods --all-namespaces -o wide >> $HOSTDIR/all-pods.txt
-kubectl get pods --all-namespaces -o yaml >> $HOSTDIR/all-pods.txt
+kubectl get pods --all-namespaces -o wide >> $LOGDIR/all-pods.txt
+echo "" >> $LOGDIR/all-pods.txt
+kubectl get pods --all-namespaces -o yaml >> $LOGDIR/all-pods.txt
 
 echo "Dumping kubectl cluster-info"
-kubectl cluster-info >> $HOSTDIR/cluserinfo
-kubectl version >> $HOSTDIR/cluserinfo
+kubectl cluster-info >> $LOGDIR/cluserinfo.txt
+kubectl version >> $LOGDIR/cluserinfo.txt
 
-echo "finished to collect log: $(date)" >> $HOSTDIR/date.txt
+echo "finished to collect log: $(date)" >> $LOGDIR/date.txt
 
-echo "compress log into $COMPRESSEDFILE"
-tar -czvf $COMPRESSEDFILE $HOSTDIR
-rm -rf $HOSTDIR
+echo "compress log into $COMPRESSEDLOGFILE"
+tar -czvf $COMPRESSEDLOGFILE -C $BASEDIR $LOGDIRNAME
+rm -rf $LOGDIR
 
 while $(true)
 do 
